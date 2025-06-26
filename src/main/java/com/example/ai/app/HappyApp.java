c47300458aa913ee201ed679dc747179057f6b86
Rigel <cailouis2003@gmail.com>
2025-06-26 18:12:07 +0800

    更新

---
+++ src/main/java/com/example/ai/app/HappyApp.java
@@ -0,0 +1,45 @@
+package com.example.ai.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
+
+@Component
+@Slf4j
+public class HappyApp {
+    private static final String SYSTEM_PROMPT = "扮演专家：";
+    private final ChatClient chatClient;
+
+    public HappyApp(ChatModel dashScopeChatModel) {
+        ChatMemory chatMemory = new InMemoryChatMemory();
+        chatClient = ChatClient.builder(dashScopeChatModel).defaultSystem(SYSTEM_PROMPT)
+                .defaultAdvisors(
+                        new MessageChatMemoryAdvisor(chatMemory),
+                        new SimpleLoggerAdvisor()
+                )
+                .build();
+    }
+    public String chat(String message,String chatId) {
+        ChatResponse response = chatClient.prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                .call()
+                .chatResponse();
+        String content = null;
+        if (response != null) {
+            content = response.getResult().getOutput().getText();
+        }
+        log.info("context:{}",content);
+        return content;
+    }
+}
---
+++ src/test/java/com/example/ai/app/HappyAppTest.java
@@ -0,0 +1,32 @@
+package com.example.ai.app;
+
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HappyAppTest {
+
+    @Resource
+    private HappyApp happyApp;
+
+    @Test
+    void testChat(){
+        String chatId = UUID.randomUUID().toString();
+
+        //第一轮对话
+        String message = "你好,我是人类";
+        String response = happyApp.chat(message,chatId);
+        Assertions.assertNotNull(response);
+
+        //第二轮对话
+        message = "我是谁";
+        response = happyApp.chat(message,chatId);
+        Assertions.assertNotNull(response);
+    }
+}
