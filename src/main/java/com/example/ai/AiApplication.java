39bdbc3e244dfb0ccc99238634a0ecc9ae95dfc9
Rigel <cailouis2003@gmail.com>
2025-07-03 00:06:28 +0800

    新增pg数据库连接

--- src/main/java/com/example/ai/AiApplication.java
+++ src/main/java/com/example/ai/AiApplication.java
@@ -1,9 +1,10 @@
 package com.example.ai;
 
+import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = PgVectorStoreAutoConfiguration.class )
 public class AiApplication {
 
     public static void main(String[] args) {
